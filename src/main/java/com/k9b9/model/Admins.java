a49235569f276ab9f30a48ad7ae15be91d172f2d
KevinB <k1@k9b9.com>
2019-03-23 18:46:50 -0400

    Converted Admin to DTO type, prior to removing old Admin class and outdated files

--- src/main/java/com/k9b9/model/Admins.java
+++ src/main/java/com/k9b9/model/Admins.java
@@ -12,6 +12,7 @@ import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.k9b9.ddb.SingleTableDdb;
 import com.k9b9.dto.Admin;
+import com.k9b9.dto.Dto;
 import com.k9b9.dto.DtoUtils;
 
 /**
@@ -32,6 +33,23 @@ public class Admins {
      * 
      * @throws JsonProcessingException
      */
+    public boolean putRootAdmin(Dto dto) {
+        // validate Admin
+        if (!ROOT_ADMIN_PKEY.equals(dto.pkey)) return false;
+        if (!ADMIN_SKEY.equals(dto.skey)) return false;
+        if ( ((Integer)dto.valueMap.get("adminLevel")).intValue() != AdminLevel.ROOT.getValue()) return false;
+        // build Item
+        Item item = DtoUtils.toItem(dto);
+        this.ddb.putItem(item);
+        // TODO add metrics for capacity used
+        return true;
+    }
+
+    /**
+     * Each call will overwrite the current Root User. There can be only one.
+     * 
+     * @throws JsonProcessingException
+     */
     public boolean putRootAdmin(Admin admin) {
         // validate Admin
         if (!ROOT_ADMIN_PKEY.equals(admin.pkey)) return false;
@@ -42,6 +60,38 @@ public class Admins {
         this.ddb.putItem(item);
         // TODO add metrics for capacity used
         return true;
+    }
+
+    /**
+     * Allow mulitiple admins with the same userID
+     */
+    public boolean putAdmin(Dto admin) {
+        // validate Admin
+        // do not add root via this method
+        if (ROOT_ADMIN_PKEY.equals(admin.pkey)) return false;
+        // must be in admin sort
+        if (!ADMIN_SKEY.equals(admin.skey)) return false;
+        // do not add root via this method
+        int adminLevel = ((Integer)admin.valueMap.get("adminLevel")).intValue();
+        boolean isValidLevel = isValidLevel(adminLevel);
+        boolean isLevelRoot = isLevelRoot(adminLevel);
+        if (isLevelRoot || !isValidLevel) return false;
+        // build Item
+        Item item = DtoUtils.toItem(admin);
+        this.ddb.putItem(item);
+        // TODO add metrics for capacity used
+        return true;
+    }
+
+    public boolean isLevelRoot(int level) {
+        return level == AdminLevel.ROOT.getValue();
+    }
+
+    public boolean isValidLevel(int level) {
+        return level == AdminLevel.ROOT.getValue()
+            || level == AdminLevel.ADMIN.getValue() 
+            || level == AdminLevel.MANAGER.getValue() 
+            || level == AdminLevel.VIEWER.getValue();
     }
 
     /**
@@ -80,6 +130,61 @@ public class Admins {
         while (iter.hasNext()) {
             Item item = iter.next();
             Admin admin = DtoUtils.toAdmin(item);
+            list.add(admin);
+        }
+        return list;
+    }
+
+    /**
+     * Returns all admins
+     */
+    public List<Dto> getAdminsDto() {
+        NameMap nameMap = new NameMap().with("#skey", "skey");
+        ValueMap valueMap = new ValueMap().withString(":skey", Admins.ADMIN_SKEY);
+        ItemCollection<ScanOutcome> items = this.ddb.table.scan("#skey = :skey", nameMap, valueMap);
+        Iterator<Item> iter = items.iterator();
+        List<Dto> list = new ArrayList<Dto>();
+        while (iter.hasNext()) {
+            Item item = iter.next();
+            Dto admin = DtoUtils.toDto(item);
+            list.add(admin);
+        }
+        return list;
+    }
+
+    public List<Dto> getAdminsByLevelDto(AdminLevel level) {
+        NameMap nameMap = new NameMap()
+            .with("#skey", "skey");
+            // .with("#jsonvalue_adminLevel","jsonvalue.adminLevel");
+        ValueMap valueMap = new ValueMap()
+            .withString(":skey", Admins.ADMIN_SKEY)
+            .withInt(":valueMap_adminLevel", level.getValue());
+        ItemCollection<ScanOutcome> items = 
+            this.ddb.table.scan("#skey = :skey AND valueMap.adminLevel = :valueMap_adminLevel", nameMap, valueMap);
+        Iterator<Item> iter = items.iterator();
+        List<Dto> list = new ArrayList<Dto>();
+        while (iter.hasNext()) {
+            Item item = iter.next();
+            Dto admin = DtoUtils.toDto(item);
+            list.add(admin);
+        }
+        return list;
+    }
+
+    public List<Dto> getAdminsByUserIdDto(String userId) {
+        NameMap nameMap = new NameMap()
+            .with("#skey", "skey");
+            // .with("#jsonvalue_adminLevel","jsonvalue.adminLevel");
+        ValueMap valueMap = new ValueMap()
+            .withString(":skey", Admins.ADMIN_SKEY)
+            .withString(":valueMap_userId", userId);
+        ItemCollection<ScanOutcome> items = 
+            this.ddb.table.scan("#skey = :skey AND valueMap.userId = :valueMap_userId", nameMap, valueMap);
+        Iterator<Item> iter = items.iterator();
+        List<Dto> list = new ArrayList<Dto>();
+        while (iter.hasNext()) {
+            Item item = iter.next();
+            Dto admin = DtoUtils.toDto(item);
             list.add(admin);
         }
         return list;
--- src/test/java/com/k9b9/model/AdminsTest.java
+++ src/test/java/com/k9b9/model/AdminsTest.java
@@ -3,10 +3,13 @@ package com.k9b9.model;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.k9b9.ddb.SingleTableDdb;
 import com.k9b9.dto.Admin;
+import com.k9b9.dto.Dto;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +26,153 @@ public class AdminsTest {
         // this.myDb = new DynDb("YourKey", "YourSecretKey", "us-east-1", "SingleTableTest");
         this.ddb = new SingleTableDdb("SingleTableTest");
     }
+
+    @Test
+    public void testCreateRootAdminDto() {
+
+        Admins admins = new Admins(this.ddb);
+
+        // test valid put
+        Map<String,Object> valueMap = new HashMap<String,Object>();
+        valueMap.put("name","John Doe");
+        valueMap.put("userId","john@server.com");
+        valueMap.put("hashedPass","Jid77(03243");
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.ROOT.getValue()));
+        
+        Dto admin = new Dto(Admins.ROOT_ADMIN_PKEY, Admins.ADMIN_SKEY, valueMap);
+        assertTrue(admins.putRootAdmin(admin));
+        System.out.println("CreateRootAdmin OK");
+
+        // test bad pkey
+        admin = new Dto("blah", Admins.ADMIN_SKEY, valueMap);
+        assertFalse(admins.putRootAdmin(admin));
+        System.out.println("CreateRootAdmin bad pkey validated OK");
+
+        // test bad skey
+        admin = new Dto(Admins.ROOT_ADMIN_PKEY, "bad-skey", valueMap);
+        assertFalse(admins.putRootAdmin(admin));
+        System.out.println("CreateRootAdmin bad skey validated OK");
+
+        // test bad level
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.VIEWER.getValue()));
+        admin = new Dto(Admins.ROOT_ADMIN_PKEY, Admins.ADMIN_SKEY, valueMap);
+        assertFalse(admins.putRootAdmin(admin));
+        System.out.println("CreateRootAdmin bad level validated OK");
+    }
+
+    @Test
+    public void testCreateRandomAdminDto() {
+        
+        Admins admins = new Admins(this.ddb);
+
+        Map<String,Object> valueMap = new HashMap<String,Object>();
+        valueMap.put("name","Jim Smith");
+        valueMap.put("userId","jim@server.com");
+        valueMap.put("hashedPass","K*#s(03243");
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.ADMIN.getValue()));
+
+        Dto admin = new Dto(java.util.UUID.randomUUID().toString(), Admins.ADMIN_SKEY, valueMap);
+        assertTrue(admins.putAdmin(admin));
+        System.out.println("CreateRandomAdmin OK");
+
+        // confirm that must be admin sort
+        admin = new Dto(java.util.UUID.randomUUID().toString(), "bad-skey", valueMap);
+        assertFalse(admins.putAdmin(admin));
+        System.out.println("CreateRandomAdmin bad skey validated OK");
+
+        // confirm root level is disallowed
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.ROOT.getValue()));
+        admin = new Dto(java.util.UUID.randomUUID().toString(), Admins.ADMIN_SKEY, valueMap);
+        assertFalse(admins.putAdmin(admin));
+        System.out.println("CreateRandomAdmin root level barred validated OK");
+
+        // confirm root pkey is disallowed
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.ADMIN.getValue()));
+        admin = new Dto(Admins.ROOT_ADMIN_PKEY, Admins.ADMIN_SKEY, valueMap);
+        assertFalse(admins.putAdmin(admin));
+        System.out.println("CreateRandomAdmin root pkey barred validated OK");
+    }
+
+    @Test
+    public void testCreateRandomManagerDto() {
+
+        Admins admins = new Admins(this.ddb);
+
+        Map<String,Object> valueMap = new HashMap<String,Object>();
+        valueMap.put("name","Tom Jones");
+        valueMap.put("userId","tom@server.com");
+        valueMap.put("hashedPass","U7&dp03243");
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.MANAGER.getValue()));
+        
+        Dto admin = new Dto(java.util.UUID.randomUUID().toString(), Admins.ADMIN_SKEY, valueMap);
+        assertTrue(admins.putAdmin(admin));
+        System.out.println("CreateRandomManager OK");
+    }
+
+    @Test
+    public void testCreateRandomViewerDto() {
+
+        Admins admins = new Admins(this.ddb);
+
+        Map<String,Object> valueMap = new HashMap<String,Object>();
+        valueMap.put("name","Bill Jones");
+        valueMap.put("userId","tom@server.com");
+        valueMap.put("hashedPass","U7&dp03243");
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.VIEWER.getValue()));
+        
+        Dto admin = new Dto(java.util.UUID.randomUUID().toString(), Admins.ADMIN_SKEY, valueMap);
+        assertTrue(admins.putAdmin(admin));
+        System.out.println("CreateRandomViewer OK");
+    }
+
+    @Test
+    public void testGetAdminsDto() {
+        Admins admins = new Admins(ddb);
+        List<Dto> list = admins.getAdminsDto();
+        for (Dto admin : list) {
+            System.out.println(admin.toString());
+        }
+    }
+
+    @Test
+    public void testGetAdminsByLevelDto() {
+        Admins admins = new Admins(ddb);
+        List<Dto> list = admins.getAdminsByLevelDto(AdminLevel.ROOT);
+        System.out.println("AdminsByLevel list size="+list.size());
+        for (Dto admin : list) {
+            System.out.println(admin.toString());
+        }
+    }
+
+    /**
+     * it is possible to add more than one user of the same UserId
+     * This should be checked
+     */
+    @Test
+    public void testGetAdminByUserIdDto() {
+        Admins admins = new Admins(this.ddb);
+        String userId = "myAdminId";
+        Map<String,Object> valueMap = new HashMap<String,Object>();
+        valueMap.put("name","Todd McMahon");
+        valueMap.put("userId",userId);
+        valueMap.put("hashedPass","U7&dp0222243");
+        valueMap.put("adminLevel",Integer.valueOf(AdminLevel.VIEWER.getValue()));
+        Dto admin = new Dto(java.util.UUID.randomUUID().toString(), Admins.ADMIN_SKEY, valueMap);
+        assertTrue(admins.putAdmin(admin));
+        List<Dto> list = admins.getAdminsByUserIdDto(userId);
+        assertTrue(list.size()>0);
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 
     @Test
     public void testCreateRootAdmin() {
